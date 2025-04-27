be07d49467deaa6fb33e0f58c5a28cbf1166b7f1
blam1921 <benjamin.lam01@sjsu.edu>
2025-04-27 13:28:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-LOGO_URL_LARGE = "https://github.com/blam1921/FULL-PROTOTYPE/blob/main/waterwatchlogo.png"
-LOGO_URL_SMALL = "https://github.com/blam1921/FULL-PROTOTYPE/blob/main/waterwatchlogo.png"
+LOGO_URL_LARGE = "https://github.com/blam1921/FULL-PROTOTYPE/blob/main/waterwatchlogo.png?raw=true"
+LOGO_URL_SMALL = "https://github.com/blam1921/FULL-PROTOTYPE/blob/main/waterwatchlogo.png?raw=true"
 
 # Set page config
 st.set_page_config(page_title="WaterWatch Community", layout="wide")
